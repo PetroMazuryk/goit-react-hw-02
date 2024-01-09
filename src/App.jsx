ec2f6fcfeeb4a8fa5f85f0eed693b29b80fa2b25
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-01-10 00:58:07 +0200

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Click on the Vite and React logos to learn more</p>
     </>
   );
 }
